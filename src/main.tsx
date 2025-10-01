ca4aa9e3a25fcdbcb74fbec8079ab486f4578b3d
Mcmxci80 <130382452+Mcmxci80@users.noreply.github.com>
2025-10-01 15:34:57 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
